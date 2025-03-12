ffe8cb70ccb25fad3852ee7275483bc7523e12c3
leticiafdepaula <leticiaferreira150998@gmail.com>
2025-03-12 15:41:12 -0300

    feat: domain

---
+++ src/main/java/com/leticia/helpDesk/domain/Status.java
@@ -0,0 +1,30 @@
+package com.leticia.helpDesk.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum Status {
+
+    ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"), ENCERRADO(2, "ENCERRADO");
+
+    private Integer codigo;
+    private String descricao;
+
+    private Status(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public static Status toEnum(Integer cod) {
+        if (cod == null) {
+            return null;
+        }
+
+        for (Status x : Status.values()) {
+            if (cod.equals(x.getCodigo())) {
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("perfil invalido");
+    }
+}
