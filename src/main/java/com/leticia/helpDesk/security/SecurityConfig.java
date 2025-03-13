3b69e809269b3631b74d8ab4f94157002073c1c0
leticiafdepaula <leticiaferreira150998@gmail.com>
2025-03-13 15:50:34 -0300

    feat: securityFilterChain

--- src/main/java/com/leticia/helpDesk/security/SecurityConfig.java
+++ src/main/java/com/leticia/helpDesk/security/SecurityConfig.java
@@ -2,17 +2,41 @@ package com.leticia.helpDesk.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        requestMatchers("/h2-console/**").permitAll()
-                .and().headers().frameOptions().sameOrigin();
-        http.authorizeHttpRequests().anyRequest();
-        return http.build();
+        return http
+                .csrf(csrf -> csrf.ignoringRequestMatchers(toH2Console()).disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers(toH2Console()).permitAll();
+                })
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
+
+
