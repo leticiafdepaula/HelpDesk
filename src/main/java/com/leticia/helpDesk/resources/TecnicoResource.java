d7adcd98519e4cc42dfc61256ed8b1c9475a5454
leticiafdepaula <leticiaferreira150998@gmail.com>
2025-03-16 13:20:06 -0300

    feat: endpoint findAll para tecnico

--- src/main/java/com/leticia/helpDesk/resources/TecnicoResource.java
+++ src/main/java/com/leticia/helpDesk/resources/TecnicoResource.java
@@ -5,10 +5,15 @@ import com.leticia.helpDesk.dtos.TecnicoDTO;
 import com.leticia.helpDesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/tecnicos")
@@ -21,8 +26,13 @@ public class TecnicoResource {
     public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = tecnicoService.findById(id);
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
-
-
     }
 
+    @GetMapping
+    public ResponseEntity<List<TecnicoDTO>> findAll() {
+        List<Tecnico> list = tecnicoService.findAll();
+        List<TecnicoDTO> listdtos = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listdtos);
+
+    }
 }
