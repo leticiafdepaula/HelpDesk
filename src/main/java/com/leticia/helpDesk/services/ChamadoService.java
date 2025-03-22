92f3851858604f860634faa38c4af052329825a9
leticiafdepaula <leticiaferreira150998@gmail.com>
2025-03-22 16:26:36 -0300

    feat: findById para chamados

---
+++ src/main/java/com/leticia/helpDesk/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package com.leticia.helpDesk.services;
+
+import com.leticia.helpDesk.domain.Chamado;
+import com.leticia.helpDesk.exception.ObjectNotFoundException;
+import com.leticia.helpDesk.repositories.ChamadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+       @Autowired
+    private ChamadoRepository chamadoRepository;
+
+       public Chamado findById(Integer id) {
+           Optional<Chamado> obj = chamadoRepository.findById(id);
+           return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado com o ID: " + id));
+       }
+}
